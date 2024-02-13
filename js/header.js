5c376b72d591bbb0c6afe60bfca6bdb5f348a3bb
Renan Silva <renansilvaytB@gmail.com>
2024-02-13 18:23:40 -0300

    feat btn mobile

--- js/header.js
+++ js/header.js
@@ -1,26 +1,32 @@
-const aListHeader = [
-  { name: 'Animais', id: 'animais' },
-  { name: 'Florestas', id: 'florestas' },
-  { name: 'Montanhas', id: 'montanhas' }
-]
+export default function initHeader() {
+  const aListHeader = [
+    { name: 'Animais', id: 'animais' },
+    { name: 'Florestas', id: 'florestas' },
+    { name: 'Montanhas', id: 'montanhas' }
+  ]
 
-function createListHeader(aLista) {
-  return aLista.map(oData => {
-    return $('<li>').append(
-      $('<a>').attr('href', `#${oData.id}`).text(oData.name)
-    )
-  })
-}
+  function createListHeader(aLista) {
+    return aLista.map(oData => {
+      return $('<li>').append(
+        $('<a>').attr('href', `#${oData.id}`).text(oData.name)
+      )
+    })
+  }
 
-const createHeader = () => {
   const header = $('<header>').addClass('menu').appendTo($('body'))
   $('<a>').addClass('logo').attr('href', '/').text('Nature').appendTo(header)
-  $('<span>').addClass('mobile-btn').appendTo(header)
-  $('<nav>')
+  const mobileBtn = $('<span>').addClass('mobile-btn').appendTo(header)
+  const navMenu = $('<nav>')
     .append($('<ul>').append(createListHeader(aListHeader)))
     .appendTo(header)
-}
 
-export default function initHeader() {
-  createHeader()
+  $(window).on('resize', () => {
+    if ($(window).width() < 768) {
+      navMenu.hide()
+      mobileBtn.addClass('show-btn')
+    } else {
+      navMenu.show()
+      mobileBtn.removeClass('show-btn')
+    }
+  })
 }
